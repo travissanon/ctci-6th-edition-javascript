7cca84b32be7d0ffcedf893b2452dc07ce7d0e5e
travissanon <traviscsanon@gmail.com>
2021-03-04 12:49:24 -0500

    [feat] add solution for case 'what if you cannot use additional data structures' & add tests

--- chapter-01/1.1 - Is Unique/isUnique.js
+++ chapter-01/1.1 - Is Unique/isUnique.js
@@ -1,3 +1,7 @@
+// 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+// Running time: O(n)
+// Running space: O(n)
 const isUnique = (str) => {
   const cache = {};
 
@@ -14,7 +18,29 @@ const isUnique = (str) => {
 };
 
 /* TESTS */
+console.log("---[isUnique]---");
 console.log(isUnique("abcd"), "true");
 console.log(isUnique("abccd"), "false");
 console.log(isUnique("bhjjb"), "false");
 console.log(isUnique("mdjq"), "true");
+
+// Running time: O(n^2)
+// Running space: O(1)?
+const isUniqueButBad = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (str[i] === str[j] && j !== i) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+/* TESTS */
+console.log("---[isUniqueButBad]---");
+console.log(isUniqueButBad("abcd"), "true");
+console.log(isUniqueButBad("abccd"), "false");
+console.log(isUniqueButBad("bhjjb"), "false");
+console.log(isUniqueButBad("mdjq"), "true");
